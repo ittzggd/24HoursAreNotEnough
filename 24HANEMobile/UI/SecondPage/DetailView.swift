d2bc2380e886a5ca98c3bdf4ee26bdc3e4f04036
maemi <dbsrl759@naver.com>
2022-12-06 00:13:10 +0900

    [fix] something more readable?

--- 24HANEMobile/UI/SecondPage/DetailView.swift
+++ 24HANEMobile/UI/SecondPage/DetailView.swift
@@ -25,14 +25,37 @@ struct DetailView: View {
     var body: some View {
         VStack(alignment: .center){
             SecondPageHeaderView()
-            CalendarView(logTimeColor: calculateLogColor(timeLogs: groupedLogs, year: date.year, month: date.month)){ day in
+            
+            CalendarView(
+                logTimeColor: calculateLogColor(
+                    timeLogs: groupedLogs,
+                    year: date.year,
+                    month: date.month
+                )
+            ) { day in
                 selectedDay = day
             }
-            PickedDataView(dailyHours: getDailyAccumulationTime(monthLogs: groupedLogs, year: date.year, month: date.month, day: selectedDay), monthlyHours: getMonthlyAccumulationTime(monthLogs: groupedLogs))
-                .position(x: 180, y: 10)
-            TagLogView(tagLogData: parsedLog(inoutLog: filteredInOutStamp))
-                .frame(width: 360, height: 250)
-                .position(x: 180, y: 0)
+            
+            PickedDataView(
+                dailyHours: getDailyAccumulationTime(
+                    monthLogs: groupedLogs,
+                    year: date.year,
+                    month: date.month,
+                    day: selectedDay
+                ),
+                monthlyHours: getMonthlyAccumulationTime(
+                    monthLogs: groupedLogs
+                )
+            )
+            .position(x: 180, y: 10)
+            
+            TagLogView(
+                tagLogData: parsedLog(
+                    inoutLog: filteredInOutStamp
+                )
+            )
+            .frame(width: 360, height: 250)
+            .position(x: 180, y: 0)
         }
         .padding()
     }
