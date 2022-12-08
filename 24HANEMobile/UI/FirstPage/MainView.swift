c445b13442ce54bb0e3f4cff45253c557fd721a5
Katherine <01kathyy@gmail.com>
2022-12-08 18:59:38 +0900

    apiTmp -> EvronmentObject

--- 24HANEMobile/UI/FirstPage/MainView.swift
+++ 24HANEMobile/UI/FirstPage/MainView.swift
@@ -8,38 +8,13 @@
 import SwiftUI
 import Foundation
 
-enum MyError: Error{
-    case invalidURLString
-    case invalidServerResponse
-}
-
-func loadMore() async -> accumationTimes{
-    let token = UserDefaults.standard.string(forKey: "Token")
-//      let request = URLRequest(url: URL(string: "https://httpbin.org/delay/2")!)
-    var request = URLRequest(url: URL(string: "https://api.24hoursarenotenough.42seoul.kr/v1/tag-log/accumulationTimes")!)
-    request.httpMethod = "GET"
-    request.allHTTPHeaderFields = [
-        "Authorization" : "Bearer \(token)"
-    ]
-    let (data, response) = try! await URLSession.shared.data(for: request)
-    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-       return accumationTimes(todayAccumationTime: 0, monthAccumationTime: 0)
-    }
-    let decodedAccumulationTimes =  try! await JSONDecoder().decode(accumationTimes.self, from: data)
-    
-    return decodedAccumulationTimes
-}
-
 struct MainView: View {
-    @State var accTime: accumationTimes
+    @EnvironmentObject var apiTmp: APIHanderTmp
+
     @Binding var inoutState: Bool
     var body: some View {
         VStack{
-//            HeaderView()
-//                .refreshable {
-//                   accTime = await loadMore()
-//                }
-//                .frame(height: 80)
+
             HStack{
                 Text("  24HoursAreNotEnough")
                     .font(.system(size: 20, weight: .light, design: .default))
@@ -53,7 +28,7 @@ struct MainView: View {
                     .foregroundColor(Color.textfordata)
                 Spacer()
             }
-            DailyView(currentTime: accTime.todayAccumationTime)
+            DailyView(currentTime: self.apiTmp.accTime.todayAccumationTime)
             Spacer(minLength: 5)
             HStack{
                 Text("    Month")
@@ -61,7 +36,7 @@ struct MainView: View {
                     .foregroundColor(Color.textfordata)
                 Spacer()
             }
-            MonthlyView(currentTime: accTime.monthAccumationTime)
+            MonthlyView(currentTime: self.apiTmp.accTime.monthAccumationTime)
             Spacer()
         }
     }
@@ -70,6 +45,6 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView(accTime: accumationTimes(todayAccumationTime: 0, monthAccumationTime: 0), inoutState: .constant(true))
+        MainView(inoutState: .constant(true))
     }
 }
