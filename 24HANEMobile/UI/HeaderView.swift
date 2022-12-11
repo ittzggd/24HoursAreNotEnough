13224d650a298088b29bfa8775d68db3505ca991
Katherine <01kathyy@gmail.com>
2022-12-11 22:26:39 +0900

    걸어가는 거북씨..

--- 24HANEMobile/UI/HeaderView.swift
+++ 24HANEMobile/UI/HeaderView.swift
@@ -14,47 +14,49 @@ struct HeaderView: View {
     @State private var isLoading = false
     
     var body: some View {
-        HStack{
-            VStack(alignment: .leading){
-                HStack{
-                    Text(" ")
-                    Image("24HaneLogo")
-                        .resizable()
-                        .frame(width: 55, height:40)
-                    Spacer()
-                    if let refresh = refresh {
-                        if isLoading {
-                            VStack(alignment: .leading){
-                                ProgressView()
-                                    .frame(width: 27, height: 27)
-                            }
-                        } else {
-                            Button(){
-                                isLoading = true
-                                Task{
-                                    await refresh()
-                                    isLoading = false
+        ZStack{
+            HStack{
+                VStack(alignment: .leading){
+                    HStack{
+                        Text(" ")
+                        Image("24HaneLogo")
+                            .resizable()
+                            .frame(width: 55, height:40)
+                        Spacer()
+                        if let refresh = refresh {
+                            if isLoading {
+                                VStack(alignment: .leading){
+                                    ProgressView()
+                                        .frame(width: 27, height: 27)
                                 }
-                            } label:{
-                                Image(systemName: "goforward")
-                                    .resizable()
-                                    .frame(width: 27, height: 27)
-                                    .foregroundColor(colorScheme == .light ? .black : .white)
-                                    .rotationEffect(.degrees(90))
+                            } else {
+                                Button(){
+                                    isLoading = true
+                                    Task{
+                                        await refresh()
+                                        isLoading = false
+                                    }
+                                } label:{
+                                    Image(systemName: "goforward")
+                                        .resizable()
+                                        .frame(width: 27, height: 27)
+                                        .foregroundColor(colorScheme == .light ? .black : .white)
+                                        .rotationEffect(.degrees(90))
+                                }
                             }
                         }
+                        Button{
+                            self.showMenu.toggle()
+                            print("Side menu")
+                        } label:{
+                            Image(systemName: "line.3.horizontal")
+                                .resizable()
+                                .frame(width: 25, height: 20)
+                                .foregroundColor(colorScheme == .light ? .black : .white)
+                        } .padding()
                     }
-                    Button{
-                        self.showMenu.toggle()
-                        print("Side menu")
-                    } label:{
-                        Image(systemName: "line.3.horizontal")
-                            .resizable()
-                            .frame(width: 25, height: 20)
-                            .foregroundColor(colorScheme == .light ? .black : .white)
-                    } .padding()
+                    Divider()
                 }
-                Divider()
             }
         }
     }
